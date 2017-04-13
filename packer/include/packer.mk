0de0792c103a00f3c2ae7e803a81cded404441b1
Alexander Gabert <alexander.gabert@gmail.com>
2017-04-13 11:05:37 +0200

    new naming convention to make AWS console happier

--- packer/include/packer.mk
+++ packer/include/packer.mk
@@ -10,9 +10,9 @@ IMAGE_TYPE = $(shell basename $(shell pwd))
 PACKER_VAR1 = -var "source_ami=$(PACKER_SOURCE_AMI)"
 PACKER_VAR2 = -var "region=$(PACKER_REGION)"
 PACKER_VAR3 = -var "instance_type=$(PACKER_INSTANCE_TYPE)"
-PACKER_VAR4 = -var "ami_name_suffix=[$(XCODE_NAME)] ($(BUILD_DATE))"
+PACKER_VAR4 = -var "ami_name_prefix=($(BUILD_DATE)) $(IMAGE_TYPE)"
 PACKER_VAR5 = -var "basic_packages=$(BASIC_PACKAGES)"
-PACKER_VAR6 = -var "ami_name_prefix=$(IMAGE_TYPE)"
+PACKER_VAR6 = -var "ami_name_suffix=[$(XCODE_NAME)]"
 
 PACKER_VARS = $(PACKER_VAR1) $(PACKER_VAR2) $(PACKER_VAR3) $(PACKER_VAR4) $(PACKER_VAR5) $(PACKER_VAR6)
 
