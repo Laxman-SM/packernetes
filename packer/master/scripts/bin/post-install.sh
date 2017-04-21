bc9ff11dca40ae2d922f42afca7a620629ed3f18
Alexander Gabert <alexander.gabert@gmail.com>
2017-04-21 21:52:59 +0200

    change script to env vars

--- packer/master/scripts/bin/post-install.sh
+++ packer/master/scripts/bin/post-install.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 #./post-install.sh
-MASTER="${1}"
-MASTER_PORT="${2:-6443}"
 
 FLANNEL="/home/ubuntu/packernetes/master/scripts/conf/kube-flannel.yaml"
+
 export KUBECONFIG="/home/ubuntu/kubernetes/admin.conf"
 
 set -e
 
 if [[ "" == "$MASTER" ]]; then
   MASTER="$HOSTNAME"
+fi
+
+if [[ "" == "$MASTER_PORT" ]]; then
+  MASTER_PORT="6443"
 fi
 
 kubectl apply \
