4bbbab1a025e1beeeb10cec16ed617a194280439
Alexander Gabert <alexander.gabert@gmail.com>
2017-04-22 23:28:12 +0200

    do not preseed docker images

--- packer/master/scripts/bin/pre-install.sh
+++ packer/master/scripts/bin/pre-install.sh
@@ -3,21 +3,5 @@
 
 set -e
 
-#
-# get the docker images to improve startup time of kubernetes master
-#
-for IMAGE in gcr.io/google_containers/kube-apiserver-amd64:v${KUBERNETES_VERSION} \
-  gcr.io/google_containers/kube-controller-manager-amd64:v${KUBERNETES_VERSION} \
-  gcr.io/google_containers/kube-scheduler-amd64:v${KUBERNETES_VERSION} \
-  gcr.io/google_containers/kube-proxy-amd64:v${KUBERNETES_VERSION} \
-  gcr.io/google_containers/etcd-amd64:3.0.17 \
-  gcr.io/google_containers/pause-amd64:3.0 \
-  gcr.io/google_containers/k8s-dns-sidecar-amd64:${K8S_DNS_VERSION} \
-  gcr.io/google_containers/k8s-dns-kube-dns-amd64:${K8S_DNS_VERSION} \
-  gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:${K8S_DNS_VERSION}
-do
-  sudo docker pull $IMAGE
-done
-
 exit 0
 
--- packer/worker/scripts/bin/pre-install.sh
+++ packer/worker/scripts/bin/pre-install.sh
@@ -9,20 +9,13 @@ echo '#'
 echo '# setting up kubernetes worker node'
 echo '#'
 
-exec /home/ubuntu/packernetes/packer/worker/scripts/bin/install.sh
+test -f /home/ubuntu/packernetes/packer/worker/scripts/bin/install.sh && \
+  exec /home/ubuntu/packernetes/packer/worker/scripts/bin/install.sh
 
 EOF
 
 sudo chmod 0755 /etc/rc.local
 sudo chown root:root /etc/rc.local
-
-#
-# get the docker images to improve startup time of kubernetes worker
-#
-for IMAGE in gcr.io/google_containers/kube-proxy-amd64:v${KUBERNETES_VERSION}
-do
-  sudo docker pull $IMAGE
-done
 
 exit 0
 
