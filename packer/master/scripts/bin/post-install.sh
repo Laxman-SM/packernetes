c6d4a174c02645e19fd9a577d2e88172e714c218
Alexander Gabert <alexander.gabert@gmail.com>
2017-04-24 12:22:44 +0200

    typo

--- packer/master/scripts/bin/post-install.sh
+++ packer/master/scripts/bin/post-install.sh
@@ -6,7 +6,7 @@ set -e
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 kubectl apply -f /root/weave.yaml
-kuhbectl apply -f /root/kubernetes-dashboard.yaml
+kubectl apply -f /root/kubernetes-dashboard.yaml
 
 kubectl apply -f /root/traefik/traefik-with-rbac.yaml
 kubectl apply -f /root/traefik/ui.yaml
