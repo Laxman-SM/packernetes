cb139565367750f006dce384e5a76bebe8668275
Alexander Gabert <alexander.gabert@gmail.com>
2017-04-25 13:55:35 +0200

    bugfix for cni.go:157] Unable to update cni config

--- packer/master/scripts/bin/post-install.sh
+++ packer/master/scripts/bin/post-install.sh
@@ -5,6 +5,12 @@ set -e
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
+# https://github.com/kubernetes/kubernetes/issues/43815
+for i in $(seq 1 200); do
+  echo -n '.'
+  sleep 1
+done
+
 kubectl apply -f /root/weave.yaml
 kubectl apply -f /root/kubernetes-dashboard.yaml
 
